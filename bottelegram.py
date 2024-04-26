58fcf881ef01ee6d80e1f1107e48e745fe9c0144
Filipe Braga Ferreira <filipebf852@poli.ufrj.br>
2024-04-26 20:54:46 -0300

    adicionei comentários pra me ensinar rsrs

--- bottelegram.py
+++ bottelegram.py
@@ -4,10 +4,13 @@ TOKEN_API = "6636899150:AAH__sSBo1QSwsJe3-3EHEt9X3inlfE_tuA"
 
 assistbot = telebot.TeleBot(TOKEN_API)
 
+# Se retornar True, vai acionar o bot via @assistbot.message_handler().
+# É com essa função que vamos decidir quais tipos de mensagem serão respondidas pelo bot.
 def verificar(mensagem):
     return True
 
 @assistbot.message_handler(func=verificar)
+
 def responder(mensagem):
     assistbot.reply_to(mensagem, "Agora eu consigo responder a qualquer mensagem que vc mandar com esse mesmo texto. Mas para isso, é necessário deixar o código rodando em algum pc.")
 

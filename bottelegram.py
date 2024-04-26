d162b9496b586dea6f91ece0f06d547e741fab7a
Juliana Barros <84809365+jubarrosousa@users.noreply.github.com>
2024-04-26 16:36:19 -0300

    Add files via upload

---
+++ bottelegram.py
@@ -0,0 +1,17 @@
+import telebot
+
+TOKEN_API = "6636899150:AAH__sSBo1QSwsJe3-3EHEt9X3inlfE_tuA"
+
+assistbot = telebot.TeleBot(TOKEN_API)
+
+def verificar(mensagem):
+    return True
+
+@assistbot.message_handler(func=verificar)
+def responder(mensagem):
+    assistbot.reply_to(mensagem, "Agora eu consigo responder a qualquer mensagem que vc mandar com esse mesmo texto. Mas para isso, é necessário deixar o código rodando em algum pc.")
+
+# Se alguém mandar alguma mensagem enquanto o código não estiver rodando, essa pessoa só será respondida se o
+# código voltar a rodar em algum pc.
+
+assistbot.polling()

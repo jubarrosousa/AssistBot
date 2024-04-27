93411dfc8a3ee3a7101386cc6d9e8abe7fa516b5
Filipe Braga Ferreira <filipebf852@poli.ufrj.br>
2024-04-26 21:50:01 -0300

    respostas por comandos adicionada

--- bottelegram.py
+++ bottelegram.py
@@ -4,17 +4,24 @@ TOKEN_API = "6636899150:AAH__sSBo1QSwsJe3-3EHEt9X3inlfE_tuA"
 
 assistbot = telebot.TeleBot(TOKEN_API)
 
+@assistbot.message_handler(commands=['youtube', 'yt'])
+def send_video(mensagem):
+	assistbot.reply_to(mensagem, "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D")
+
 # Se retornar True, vai acionar o bot via @assistbot.message_handler().
 # É com essa função que vamos decidir quais tipos de mensagem serão respondidas pelo bot.
 def verificar(mensagem):
     return True
 
 @assistbot.message_handler(func=verificar)
-
 def responder(mensagem):
     assistbot.reply_to(mensagem, "Agora eu consigo responder a qualquer mensagem que vc mandar com esse mesmo texto. Mas para isso, é necessário deixar o código rodando em algum pc.")
 
 # Se alguém mandar alguma mensagem enquanto o código não estiver rodando, essa pessoa só será respondida se o
 # código voltar a rodar em algum pc.
 
-assistbot.polling()
+def main(): 
+    assistbot.infinity_polling()
+
+if __name__ == "__main__":
+    main()
